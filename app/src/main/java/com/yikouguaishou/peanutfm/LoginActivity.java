63ae1868be579e2131d31bec51e773da95bece1a
Administrator <351898472@qq.com>
2017-01-11 17:42:15 +0800

    第三方登录

--- app/src/main/java/com/yikouguaishou/peanutfm/LoginActivity.java
+++ app/src/main/java/com/yikouguaishou/peanutfm/LoginActivity.java
@@ -13,13 +13,12 @@ import java.util.HashMap;
 import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.sina.weibo.SinaWeibo;
 import cn.sharesdk.tencent.qq.QQ;
-import cn.sharesdk.tencent.weibo.TencentWeibo;
 
 public class LoginActivity extends AppCompatActivity {
 
     private String iconurl;
-    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,20 +45,14 @@ public class LoginActivity extends AppCompatActivity {
                 Log.e("TAG", "onComplete: ======>登录成功");
 
 
-                if (platform.getName().equals(QQ.NAME)){
                     iconurl = hashMap.get("figureurl_qq_2").toString();
-                }
-                else if (platform.getName().equals(TencentWeibo.NAME))
-                {
-
-                }
 
 
-                username = platform.getDb().getUserName();
-//                backToMine(iconurl);
+                String username=platform.getDb().getUserName();
+
                 Intent intent = new Intent();
                 intent.putExtra("iconurl", iconurl);
-                intent.putExtra("username", username);
+                intent.putExtra("username",username);
                 setResult(200,intent);
                 finish();
             }
@@ -74,10 +67,46 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-//        qq.showUser(null);
+        platform.showUser(null);
 
 
     }
+
+//    public void SinaLogin(View view) {
+//        ShareSDK.initSDK(this);
+//        Platform platform=ShareSDK.getPlatform(this, SinaWeibo.NAME);
+//        platform.setPlatformActionListener(new PlatformActionListener() {
+//            @Override
+//            public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+//                Log.e("TAG", "onComplete: ======>登录成功");
+//
+//
+//
+//
+//                    iconurl = hashMap.get("avatar_large").toString();
+//
+//
+//                String username=platform.getDb().getUserName();
+//
+//                Intent intent = new Intent();
+//                intent.putExtra("iconurl", iconurl);
+//                intent.putExtra("username",username);
+//                setResult(200,intent);
+//                finish();
+//            }
+//
+//            @Override
+//            public void onError(Platform platform, int i, Throwable throwable) {
+//                Log.e("TAG", "onError: ======>登录失败！原因是:"+throwable.getMessage());
+//            }
+//
+//            @Override
+//            public void onCancel(Platform platform, int i) {
+//
+//            }
+//        });
+//        platform.showUser(null);
+//    }
 
 //    private void backToMine(String iconurl){
 //        Intent intent=new Intent(this, MineFragment.class);
